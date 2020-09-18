89364531bda47ba6f3568927e8d030d9cf80293e
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-18 02:22:04 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+  attr_accessor :name, :artist, :artist_name
+  @@all = []
+  
+  
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.new_by_filename(filename)
+    split_filename = filename.split(" - ")
+    song = self.new(split_filename[1])
+    
+    artist = Artist.find_or_create_by_name(split_filename[0])
+    
+    song.artist = artist
+    
+    artist.add_song(song)
+    
+     song
+  end
+  
+  def artist_name(artist_name)
+    
+  end
+
+    
+  
+  
+end
